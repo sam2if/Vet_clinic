918f68f0e0a06b4ac98479933328fd33260eabf5
Samuel <samif3443@gmail.com>
2023-11-20 11:59:25 -0800

    Add data to queries.sql file

--- queries.sql
+++ queries.sql
@@ -5,3 +5,4 @@ SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
+SELECT * FROM animals WHERE (weight_kg >= 10.4 AND weight_kg <= 17.3);
