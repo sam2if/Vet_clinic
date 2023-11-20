45842d2cebe83efb35adcfe914a942c42b16cac2
Samuel <samif3443@gmail.com>
2023-11-20 11:32:21 -0800

    Add data to data.sql file

--- data.sql
+++ data.sql
@@ -1,1 +1,2 @@
 INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Agumon', '2020-02-03', 0, FALSE, 10.23);
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Gabumon', '2018-11-15', 8, TRUE, 2);
