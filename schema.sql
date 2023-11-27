0727396a4bc78a61bec83e2e5107aab1a97cd28c
Samuel <samif3443@gmail.com>
2023-11-27 05:24:37 -0800

    Add queries to schema.sql

--- schema.sql
+++ schema.sql
@@ -21,3 +21,7 @@ ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 INSERT INTO visits (animals_id, vets_id, date_of_visits) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
 -- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
 insert into owners (full_name, age, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;  
+
+
